804ec7e888e83cecf333be25f870895ee1c40a04
megaivotu09 <xenosvn123@gmail.com>
2025-07-11 22:18:11 +0700

    Update AttributeManager.java

--- src/main/java/com/yourname/tutien/mechanics/AttributeManager.java
+++ src/main/java/com/yourname/tutien/mechanics/AttributeManager.java
@@ -6,6 +6,7 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
+
 import java.util.UUID;
 
 public class AttributeManager {
@@ -14,41 +15,33 @@ public class AttributeManager {
     private static final UUID DEFENSE_MODIFIER_UUID = UUID.fromString("b8b3b3e6-71d5-45a2-8447-5d5a6b35368b");
     private static final UUID DAMAGE_MODIFIER_UUID = UUID.fromString("c8c3c3e6-71d5-45a2-8447-5d5a6c35368c");
 
-    public AttributeManager(TuTienPlugin plugin) { this.plugin = plugin; }
+    public AttributeManager(TuTienPlugin plugin) {
+        this.plugin = plugin;
+    }
 
     public void updatePlayerAttributes(Player player) {
         PlayerData data = plugin.getPlayerDataManager().getPlayerData(player);
         if (data == null) return;
+
         StatManager statManager = new StatManager(data.getTuLuyenInfo());
         double maxHealth = statManager.getMaxHealth();
         double defense = statManager.getDefense();
         double attackDamage = statManager.getAttackDamage();
-        
-        AttributeInstance healthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-        if (healthAttribute != null) {
-            healthAttribute.getModifiers().stream()
-                    .filter(m -> m.getUniqueId().equals(HEALTH_MODIFIER_UUID))
-                    .forEach(healthAttribute::removeModifier);
-            AttributeModifier healthModifier = new AttributeModifier(HEALTH_MODIFIER_UUID, "tutien_health", maxHealth - 20, AttributeModifier.Operation.ADD_NUMBER);
-            healthAttribute.addModifier(healthModifier);
-        }
 
-        AttributeInstance defenseAttribute = player.getAttribute(Attribute.GENERIC_ARMOR);
-        if (defenseAttribute != null) {
-            defenseAttribute.getModifiers().stream()
-                    .filter(m -> m.getUniqueId().equals(DEFENSE_MODIFIER_UUID))
-                    .forEach(defenseAttribute::removeModifier);
-            AttributeModifier defenseModifier = new AttributeModifier(DEFENSE_MODIFIER_UUID, "tutien_defense", defense, AttributeModifier.Operation.ADD_NUMBER);
-            defenseAttribute.addModifier(defenseModifier);
-        }
-        
-        AttributeInstance damageAttribute = player.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
-        if (damageAttribute != null) {
-            damageAttribute.getModifiers().stream()
-                    .filter(m -> m.getUniqueId().equals(DAMAGE_MODIFIER_UUID))
-                    .forEach(damageAttribute::removeModifier);
-            AttributeModifier damageModifier = new AttributeModifier(DAMAGE_MODIFIER_UUID, "tutien_damage", attackDamage, AttributeModifier.Operation.ADD_NUMBER);
-            damageAttribute.addModifier(damageModifier);
-        }
+        updateAttribute(player, Attribute.GENERIC_MAX_HEALTH, HEALTH_MODIFIER_UUID, "tutien_health", maxHealth - 20);
+        updateAttribute(player, Attribute.GENERIC_ARMOR, DEFENSE_MODIFIER_UUID, "tutien_defense", defense);
+        updateAttribute(player, Attribute.GENERIC_ATTACK_DAMAGE, DAMAGE_MODIFIER_UUID, "tutien_damage", attackDamage);
+    }
+
+    private void updateAttribute(Player player, Attribute attribute, UUID uuid, String name, double value) {
+        AttributeInstance instance = player.getAttribute(attribute);
+        if (instance == null) return;
+
+        instance.getModifiers().stream()
+                .filter(m -> m.getUniqueId().equals(uuid))
+                .forEach(instance::removeModifier);
+
+        AttributeModifier modifier = new AttributeModifier(uuid, name, value, AttributeModifier.Operation.ADD_NUMBER);
+        instance.addModifier(modifier);
     }
 }
